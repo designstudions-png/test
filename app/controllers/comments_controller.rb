69fbdf631c91fda347db10a61b4c5a0d1a1ab74f
dn1codegen <dn1codegen@gmail.com>
2026-01-06 15:14:38 +0300

    add images in comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -28,6 +28,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:body, images: [])
   end
 end
